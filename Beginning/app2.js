16229b5d21f0abd1fc381a7fc16b2eb0da9aef81
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-03 15:40:35 +0300

    ES6 TRICKS

---
+++ Beginning/app2.js
@@ -0,0 +1,33 @@
+        var a;
+        console.log(a);
+// ------------------------
+        var name = "ahmet rasim";
+        console.log(name);
+        name = "ahmet";
+        console.log(name);
+// ------------------------
+        let name2 = "Mustafa Murat";
+        console.log(name2);
+        name2 = "Murat";
+// ------------------------
+        let b = 20;
+        if(b == 10){
+            let b = 30;
+            console.log(b);
+        }
+        else
+        {
+            console.log(b + " Hala böyle");
+        }
+// ------------------------
+        const c = [1,2,3,4,5];
+        console.log(c);
+        c.push(6,7,8,9);
+        console.log(c);
+        c.push(7,7,7);
+        console.log(c);
+// ------------------------
+        // USE "LET" AND "CONST"
+        
+
+        
