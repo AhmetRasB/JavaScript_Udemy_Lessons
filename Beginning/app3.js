4621835281a4811c1366560f6efaba28954d4bb1
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-03 16:05:12 +0300

    Daily Last JS

---
+++ Beginning/app3.js
@@ -0,0 +1,23 @@
+// --------------------------
+    let value;
+    value = 123;
+    console.log(value);
+    console.log(typeof value);
+// --------------------------
+    value= String(123);
+    console.log(value);
+    value= String(3.14);
+    console.log(value);
+    value= String(false);
+    console.log(value);
+    value= String(function(){console.log()});
+    value= String([1,2,3,4]);
+    value = (10).toString();
+    console.log(typeof value);
+// --------------------------
+    value = Number("123");
+    console.log(value);
+// -------------------------
+    value = Number("3.14");
+    console.log(typeof value);
+    
