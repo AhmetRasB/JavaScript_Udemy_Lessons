4c99878fc2e4e856ebe5d4d8a48e5bb00144fab8
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-03 16:21:33 +0300

    Math Methods

---
+++ Beginning/app4.js
@@ -0,0 +1,24 @@
+let value;
+const value1 = 10;
+const value2 = 4;
+value = value1 + value2;
+console.log(value);
+value = value1 - value2;
+console.log(value);
+value = value1 * value2;
+console.log(value);
+value = value1 / value2;
+console.log(value);
+value = value1 % value2;
+console.log(value);
+// -------------------------
+    value = Math.PI;
+    console.log(value);
+    value = Math.E;
+    console.log(value);
+
+    value= Math.round(3.6);
+    console.log(value);
+    value= Math.ceil(3.2); 
+    console.log(value);
+
