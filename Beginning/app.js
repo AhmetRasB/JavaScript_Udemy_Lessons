2cb64e11fdfe90fdf19753be234ed92925b98aac
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-03 15:13:36 +0300

    Variables

--- Beginning/app.js
+++ Beginning/app.js
@@ -2,3 +2,19 @@
 // console.log(a);
 // console.log(["Ahmet","Mehmet"]);
 // console.log(typeof a);
+
+
+// Primitive 
+// ------------------------------------
+    var a = 10;
+    console.log(a);
+    console.log(typeof a);
+// ------------------------------------
+    var b =3.14;
+    console.log(b);
+    console.log(typeof b);
+// ------------------------------------
+    var name = "Mustafa";
+    console.log(name);
+    console.log(typeof name);
+// ------------------------------------
