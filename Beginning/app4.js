f4c805d9897e5d6b9054f44b67d3db959cb80b0a
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-03 16:34:24 +0300

    Next

--- Beginning/app4.js
+++ Beginning/app4.js
@@ -1,24 +1,43 @@
-let value;
-const value1 = 10;
-const value2 = 4;
-value = value1 + value2;
-console.log(value);
-value = value1 - value2;
-console.log(value);
-value = value1 * value2;
-console.log(value);
-value = value1 / value2;
-console.log(value);
-value = value1 % value2;
-console.log(value);
+// -------------------------
+    let value;
+    const value1 = 10;
+    const value2 = 4;
+    value = value1 + value2;
+    console.log(value);
+    value = value1 - value2;
+    console.log(value);
+    value = value1 * value2;
+    console.log(value);
+    value = value1 / value2;
+    console.log(value);
+    value = value1 % value2;
+    console.log(value);
 // -------------------------
     value = Math.PI;
     console.log(value);
     value = Math.E;
     console.log(value);
-
+// -------------------------
     value= Math.round(3.6);
     console.log(value);
     value= Math.ceil(3.2); 
     console.log(value);
+// -------------------------
+    value= Math.sqrt(16);
+    console.log(value);
+    value= Math.abs(-10);
+    console.log(value);
+// -------------------------
+    value= Math.pow(8,3);
+    console.log(value);
+// -------------------------
+    value = Math.max(10,-1,100,32);
+    console.log(value);
+    value = Math.min(10,-1,100,32);
+    console.log(value);
+// -------------------------
+    value = Math.floor(Math.random()* 20 +1 );
+    console.log(value);
+
+// -------------------------
 
