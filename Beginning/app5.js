05f57662240f221219cc4e1f0c0c882e02dcd60b
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-04 11:33:32 +0300

    daily first

--- Beginning/app5.js
+++ Beginning/app5.js
@@ -1,2 +1,55 @@
 // ---------------------
-    let value
+    let value;
+    const firstname = "Lois";
+    const lastName = "Armstrong";
+    const langs = "Java,Python,C++";
+    value = firstname + " " + lastName;
+    console.log(value);
+   
+// ---------------------------
+    value = "Ahmet Rasim";
+    value = value + " Beyhan";
+    console.log(value);
+    value = "Ahmet Rasim";
+    value += " Beyhan";
+    console.log(value);
+
+// ---------------------------
+    value = firstname.concat(" ",lastName," ","Caz");
+    console.log(value);
+// ---------------------------
+    value = firstname.toLowerCase();
+    console.log(value);
+// ---------------------------
+    value = firstname.toUpperCase();
+    console.log(value);
+// ---------------------------
+    value = firstname[0];
+    console.log(value);
+    value = firstname[1];
+    console.log(value);
+    value = firstname[2];
+    console.log(value);
+    value = firstname[3];
+    console.log(value);
+// ---------------------------
+    value = firstname.indexOf("L");
+    console.log(value);
+    value = firstname.indexOf("o");
+    console.log(value);
+    value = firstname.indexOf("z");
+    console.log(value);
+// ---------------------------
+    value = firstname.charAt(0);
+    console.log(value);
+    value = firstname.charAt(1);
+    console.log(value);
+    value = firstname.charAt(2);
+    console.log(value);
+    value = firstname.charAt(3);
+    console.log(value);
+    
+   
+
+// ---------------------------
+// ---------------------------
