23f1f85557febf78edbff5d4cd5373821c5f5ac4
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-05 13:24:46 +0300

    First Commit for Objects

---
+++ Beginning/app8.js
@@ -0,0 +1,18 @@
+const programmer = {
+    name: "Ahmet Rasim Beyhan",
+    age: 25,
+    email: "ahmet@gmail.com"
+    langs:[
+        "Python",
+        "Java",
+        "JavaScript"
+    ],
+    address:[
+        city: "Ontario",
+        street: "Bloor"
+    ],
+    work : function(){
+        console.log("Programcı Çalışıyor");
+    }
+    
+}
