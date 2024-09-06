7f2f08b53d1e4c68f0183ef5994cd1b1f5cee429
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-06 13:38:34 +0300

    Function Null Testing

--- Beginning/app12.js
+++ Beginning/app12.js
@@ -1,11 +1,25 @@
 // ----------------------------------
     function merhaba(){
+       
         console.log("Merhaba");
     }
     merhaba();
 // ----------------------------------
     function test(name,age){
+        if(typeof name === "undefined") name = "Bilgi Yok";
+        if(typeof age === "undefined") age = "Bilgi Yok";
         console.log(`İsim ${name} Yaş ${age}`);
     }
     test("ahmet",25);
+    test("mehmet",20);
+    test();
 // ----------------------------------
+    function test2(name2 = "Bilgi Yok",age2 = "Bilgi Yok"){
+        console.log(`İsim ${name2} Yaş ${age2}`);
+    }
+    test2("ahmet",25);
+    test2("mehmet",20);
+    test2();
+    test2("Kadir");
+
+// ---------------------------------
