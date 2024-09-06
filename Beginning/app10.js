fbbcd508ef82bbf9833d81c0691a9460cef77ae5
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-06 13:02:12 +0300

    Arithmethic Operators

--- Beginning/app10.js
+++ Beginning/app10.js
@@ -1,4 +1,16 @@
 // --------------------------
-console.log(2==2);
-console.log("js"=="java");
-console.log(2 == "2");
+    console.log(2==2);
+    console.log("js"=="java");
+    console.log(2 == "2");
+    console.log(2==="2");
+    console.log(4>2);
+// --------------------------
+    console.log(2!=4);
+    console.log(2<=4);
+    console.log(2>=4);
+// --------------------------
+    console.log(!(2 == 2));
+    console.log((2 == 2 ) && (2<4));
+    console.log((2<=4)&&(5<4));
+    
+
