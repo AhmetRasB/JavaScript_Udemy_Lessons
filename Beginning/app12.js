dacd8e4d1090a88c359b831aaec7b289f4220165
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-06 13:22:03 +0300

    Nice

---
+++ Beginning/app12.js
@@ -0,0 +1,11 @@
+// ----------------------------------
+    function merhaba(){
+        console.log("Merhaba");
+    }
+    merhaba();
+// ----------------------------------
+    function test(name,age){
+        console.log(`İsim ${name} Yaş ${age}`);
+    }
+    test("ahmet",25);
+// ----------------------------------
