035735cedd87d2bb09c3e8c675f525bc11d5209e
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-06 13:05:29 +0300

    Ternary Operators

--- Beginning/app10.js
+++ Beginning/app10.js
@@ -12,5 +12,20 @@
     console.log(!(2 == 2));
     console.log((2 == 2 ) && (2<4));
     console.log((2<=4)&&(5<4));
-    
+// --------------------------
+    if(5<4){
+        console.log("Küçük");
+    }
+    else{
+        console.log("Büyük")
+    }
+// -------------------------
+    var error = "selam"; 
+    if(error == "selams"){
+            console.log("selam");
+    }else if(error == true && 5 == 5){
+            console.log("Selamm");
+    }else{
+        console.log("false");
+    }
 
