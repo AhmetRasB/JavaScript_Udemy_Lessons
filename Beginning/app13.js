9c55a310cde11ac5a84fdae0553faa7c1fd2f510
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-11 19:06:26 +0300

    melih

---
+++ Beginning/app13.js
@@ -0,0 +1,15 @@
+// -----------------------------------------------
+    const langs = ["Python","JavaScript","Java"];
+    langs.forEach(function(langs){
+        console.log(langs);
+    });
+// -----------------------------------------------
+    const users = [
+        {name:"Mustafa",age:25},
+        {name:"Zeynep",age:15},
+        {name:"Zeliha",age:16},
+    ];
+    const names = users.map(function(user){
+        return users.name;
+    });
+    console.log(names);
