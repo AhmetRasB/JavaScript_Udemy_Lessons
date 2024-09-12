522d8e24337585e02a68eb02437ee795a38fea20
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-12 14:19:06 +0300

    Window Properities

--- Beginning/app14.js
+++ Beginning/app14.js
@@ -1,1 +1,26 @@
-console.log(this);
+// -----------------------------------
+    // console.log(this);
+    // console.log(this.alert);
+    // console.log(window);
+// -----------------------------------
+    //console.log(alert());
+    // const cevap = confirm("Emin Misiniz?");
+    // if(cevap){
+    //     console.log("selam");
+    // }
+    // else
+    //     console.log(alert("Hayır Dediniz!"));
+// -----------------------------------
+    // const cevap = prompt("2 + 2 = ? ");
+    // if(cevap==(2+2)){
+    //     console.log("Doğru");
+    // }
+// ------------------------------------
+    let value;
+    value = window;
+    console.log(value);
+    value = window.location;
+    console.log(value);
+    value = window.location.hostname;
+    console.log(value);
+    
