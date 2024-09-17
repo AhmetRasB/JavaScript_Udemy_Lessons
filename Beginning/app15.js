94baab6bc723b39692d42ee31695db8e8f3ae44d
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-17 09:53:05 +0300

    Daily First

---
+++ Beginning/app15.js
@@ -0,0 +1,14 @@
+// --------Global--Scope-------------
+    function a(){
+        // Function Scope
+    }
+    // if(){
+    //     // Block Scope
+    // }
+// ----------------------------------
+    var value1 = 10;
+    let value2 = 20;
+    const value3 = 30;
+
+    console.log(value1, " ", value2,  " ", value3);
+// ----------------------------------
