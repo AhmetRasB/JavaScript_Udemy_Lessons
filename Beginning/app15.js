9dc1bc4b2962bcd9487407b97bf2e5a313d14c23
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-17 10:06:34 +0300

    Last Example

--- Beginning/app15.js
+++ Beginning/app15.js
@@ -12,3 +12,20 @@
 
     console.log(value1, " ", value2,  " ", value3);
 // ----------------------------------
+    function Func(){
+        var value1 = 40;
+        let value2 = 20;
+        const value3 = 60;
+
+        console.log(value1, " ", value2,  " ", value3);
+    }
+    Func();
+    if(true){
+        var value1 = 10;
+        let value2 = 20;
+        const value3 = 30;
+        console.log(value1,value2,value3);
+    }
+    console.log(value1);
+    console.log(value2); // app15.js:30 Uncaught ReferenceError: b is not defined when variabla is b 
+    console.log(value3);
