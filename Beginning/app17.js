5c16cf182c553153d58d80a64102b000505cebdf
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-19 12:16:52 +0300

    Classa göre seçim

--- Beginning/app17.js
+++ Beginning/app17.js
@@ -1,1 +1,28 @@
 // Choosing Id for Elements
+let element;
+
+element = document.getElementById("todo-form");
+
+console.log(element);
+
+element = document.getElementById("tasks-title");
+
+console.log(element);
+// - Class'a göre seçme
+
+element = document.getElementsByClassName("list-group-item");
+
+console.log(element);
+
+element = document.getElementsByClassName("card-header");
+
+console.log(element);
+// - Tag a göre seçme
+
+element = document.getElementsByTagName("li");
+
+console.log(element);
+
+element = document.getElementsByTagName("div");
+
+console.log(element);
