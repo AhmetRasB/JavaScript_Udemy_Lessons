dc41a55a1f77d55f837fcc44373b1b79e84a2db7
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-21 13:11:07 +0300

    Choosing Elements And Change style

---
+++ Beginning/app19.js
@@ -0,0 +1,7 @@
+const ele = document.querySelector("#clear-todos");
+
+console.log(ele.id);
+console.log(ele.className);
+console.log(ele.classList);
+console.log(ele.classList[1]);
+console.log(ele.classList[0]);
