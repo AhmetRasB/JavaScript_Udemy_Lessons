5bfab6f9d4c5aa543ce5a19e9e9bb2cbf1eaf38e
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-21 13:15:35 +0300

    ---------------------Stlye ve Element Özelliklerini Değişmek------------------------------

--- Beginning/app19.js
+++ Beginning/app19.js
@@ -1,10 +1,14 @@
-const ele = document.querySelector("#clear-todos");
+    const ele = document.querySelector("#clear-todos");
+// ---------------------------------------------------
+//     console.log(ele.id);
+//     console.log(ele.className);
+//     console.log(ele.classList);
+//     console.log(ele.classList[1]);
+//     console.log(ele.classList[0]);
+//     console.log(ele.textContent);
+//     console.log(ele.innerHTML);
+//     console.log(ele.href);
+// // ---------------------------------------------------
+//     console.log(ele.style);
+// ---------------------Stlye ve Element Özelliklerini Değişmek------------------------------
 
-console.log(ele.id);
-console.log(ele.className);
-console.log(ele.classList);
-console.log(ele.classList[1]);
-console.log(ele.classList[0]);
-console.log(ele.textContent);
-console.log(ele.innerHTML);
-console.log(ele.href);
