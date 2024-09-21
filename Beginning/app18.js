cb6eb5db88cea570e15f19bdb2896d6f1a463aca
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-21 12:33:09 +0300

    Dom Continue

---
+++ Beginning/app18.js
@@ -0,0 +1,26 @@
+    const element = document.querySelector("#clear-todos");
+    // console.log(element.id);
+    // console.log(element.className);
+    // console.log(element.classList);
+    // console.log(element.classList[1]);
+    // console.log(element.textContent); //Same
+    // console.log(element.innerHTML); // Same
+    // console.log(element.href);
+    // console.log(element.style);
+// -----------------------------------------
+    // element.className = "btn btn-primary";
+    // console.log(element);
+    // element.style.color= "#000";
+    // element.style.marginLeft = "50px";
+    // element.href = "https://google.com";
+    // element.target  = "_blank";
+// -----------------------------------------
+    // element.textContent = "Silin";
+    // // element.textContent = "<span>Silin</span>"; Wrong
+    // element.innerHTML = "<span style = 'color:green'>Silin</span>";
+// -----------------------------------------
+    // const elements = document.querySelectorAll(".list-group-item"); 
+    // elements.forEach(function(el){
+    //     el.style.color = "red";
+    //     el.style.backgroundColor = "yellow";
+    // });
