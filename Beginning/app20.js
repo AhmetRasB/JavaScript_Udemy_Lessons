532ea178e6b5b26b3f123ab152e9ef609d0487a2
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-28 13:34:00 +0300

    asdas

---
+++ Beginning/app20.js
@@ -0,0 +1,7 @@
+let value;
+const todoList = document.querySelector(".list-group");
+const todo = document.querySelector(".list-group-item:nth-child(2)");
+const cardrow = document.querySelector(".card.row");
+
+
+console.log(value);
