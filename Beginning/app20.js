8ffb6c5b867c0b248fbddecd6bef76b86f612194
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-28 13:24:32 +0300

    Changed Files

---
+++ Beginning/app20.js
@@ -0,0 +1,5 @@
+const paragraphs = document.querySelectorAll("p");
+// paragraphs[0] is the first <p> element
+// paragraphs[1] is the second <p> element, etc.
+alert(paragraphs[0].nodeName);
+ console.log(paragraphs);
