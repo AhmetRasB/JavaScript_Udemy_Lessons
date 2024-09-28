b9227d2540a512844ca29fbf28f5d229f9469a2f
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-28 13:24:54 +0300

    Change effected

--- Beginning/app20.js
+++
@@ -1,5 +0,0 @@
-const paragraphs = document.querySelectorAll("p");
-// paragraphs[0] is the first <p> element
-// paragraphs[1] is the second <p> element, etc.
-alert(paragraphs[0].nodeName);
- console.log(paragraphs);
