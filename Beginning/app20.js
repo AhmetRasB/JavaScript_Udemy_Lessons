21c7efea585354e9dec624455830666072837850
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-09-29 11:00:01 +0300

    Last Commit todo child,parent

--- Beginning/app20.js
+++ Beginning/app20.js
@@ -2,6 +2,23 @@ let value;
 const todoList = document.querySelector(".list-group");
 const todo = document.querySelector(".list-group-item:nth-child(2)");
 const cardrow = document.querySelector(".card.row");
+value = todoList;
+value = todo;
+value = cardrow;
+// Child Nodes
+value = todoList.childNodes;
+value = todoList.childNodes[2];
+//value = todoList.children;
+//value = todoList.children[1];
 
 
+value = cardrow;
+value = cardrow.children;
+value = cardrow.children[2].children[1].textContent;
+value = cardrow.parentElement;
+value = cardrow.parentElement.parentElement;
+value = todoList.children[1].parentElement;
+value = todo.previousElementSibling;
+value = todo.nextElementSibling.nextElementSibling;
+value = todo.previousElementSibling;
 console.log(value);
