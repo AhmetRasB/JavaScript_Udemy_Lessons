31115bbd5520b4465126069ac340eff3e5214330
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-10-04 11:42:25 +0300

    Last Commit

--- Beginning/app22.js
+++ Beginning/app22.js
@@ -1,12 +1,11 @@
-// const cardbody = document.querySelectorAll(".card-body")[1];
-// const newElement = document.createElement("h3");
+const cardbody = document.querySelectorAll(".card-body")[1];
+const newElement = document.createElement("h3");
 
-// newElement.className = "card-title";
-// newElement.id = "tasks-title";
-// newElement.textContent = "Yeni Todolar";
+newElement.className = "card-title";
+newElement.id = "tasks-title";
+newElement.textContent = "Yeni Todolar";
 
-// const oldElement = document.querySelector("#task-title");
-// cardbody.replaceChild(newElement,oldElement);
-// console.log(cardbody);
-// console.log(newElement);
-// console.log(oldElement);
+const oldElement = document.querySelector("#task-title");
+cardbody.replaceChild(newElement,oldElement);
+
+console.log(newElement);
