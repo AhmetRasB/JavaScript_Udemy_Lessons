81a3c7bc9011932ca025e429f80b086ecb266c8d
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-10-04 11:35:13 +0300

    NodeList

---
+++ Beginning/app22.js
@@ -0,0 +1,6 @@
+const todoList = document.querySelector("ul.lis-group");
+const todos = document.querySelectorAll("li.list-group-item");
+todos[0].remove;
+console.log(todoList);
+console.log(todos);
+todoList.removeChild(todoList.lastElementChild);
