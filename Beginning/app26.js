4835306104455ea94b80d046bab1013983643d7b
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-10-17 13:49:51 +0300

    All Local Storage Events and Array Type of Decleration Complete

--- Beginning/app26.js
+++ Beginning/app26.js
@@ -1,5 +1,45 @@
-﻿// localStorage.setItem("tekrar","burpee");
+﻿// // localStorage.setItem("tekrar","burpee");
+//
+// const value = localStorage.getItem("haraket");
+// console.log(value);
+// console.log(typeof value);
+// localStorage.clear();
+//
+// localStorage.setItem("haraket","burpee");
+// localStorage.setItem("tekrar","50");
+// if(localStorage.getItem("sport") === null){
+//     console.log("Yok");
+// }
+// else  
+//     console.log(sessionStorage.getItem(value));
+//
+// const todos = ["Todo 1", "Todo 2", "Todo 3"];
+//
+// // localStorage.setItem("todos",JSON.stringify(todos));
+//
+// const value =  JSON.parse(localStorage.getItem("todos"));
+// console.log(value);
 
-const value = localStorage.getItem("haraket");
-console.log(value);
-console.log(typeof value);
+const form = document.getElementById("todo-form");
+const todoInput = document.getElementById("todo");
+
+form.addEventListener("submit",addTodo);
+function addTodo(e){
+    const value = todoInput.value;
+    
+    let todos;
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    }
+    else{
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    
+    todos.push(value);
+    localStorage.setItem("todos",JSON.stringify(todos));
+    
+    
+    
+    
+    e.preventDefault();
+}
