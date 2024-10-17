ab4ea111ce59096fd1e0bdc0a11563653933805d
Ahmet Beyhan <ahmetrasimbayhan@gmail.com>
2024-10-17 12:55:27 +0300

    Session Storage Test

--- Beginning/app25.js
+++ Beginning/app25.js
@@ -1,1 +1,22 @@
-// Session Storage - Key and Value
+
+// Choosing Buttons
+const add = document.querySelector("#add");
+const del = document.querySelector("#delete");
+const clear = document.querySelector("#clear");
+
+// Choosing Inputs
+
+const addKey = document.querySelector("#addkey");
+const addvalue = document.querySelector("#addvalue");
+const deletekey = document.querySelector("#deletekey");
+
+add.addEventListener("click",addItem);
+add.addEventListener("click",deleteItem);
+add.addEventListener("click",clearItem);
+function addItem(e){
+    sessionStorage.setItem(addkey.value,addvalue.value);
+}
+function deleteItem(e){}
+function clearItem(es){
+    
+}
